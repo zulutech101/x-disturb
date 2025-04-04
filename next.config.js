c3799cc498368950b9cb6c1d77b4ccba3d517264
Zuulu Tech <zuulu.tech@gmail.com>
2025-04-04 13:23:01 +0300

    update package

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
-};
-
-module.exports = nextConfig;
+  /* config options here */
+}
+ 
+module.exports = nextConfig
