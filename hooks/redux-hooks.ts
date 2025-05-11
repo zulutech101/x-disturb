8c31cc3f31db422228c47b9f511288adff85487c
ET-Zulu <abushareserved@gmail.com>
2025-05-11 23:17:03 +0300

    fixed build error

--- hooks/redux-hooks.ts
+++ hooks/redux-hooks.ts
@@ -1,5 +1,5 @@
 import { type TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
-import type { RootState, AppDispatch } from "@/lib/store"
+import type { RootState, AppDispatch } from "@/store/store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch
